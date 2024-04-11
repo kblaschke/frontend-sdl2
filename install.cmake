36de627aa4854a9becd9d2a00473c044a39489ca
Kai Blaschke <kai.blaschke@kb-dev.net>
2024-04-11 18:32:29 +0200

    WIP: Packaging improvements

--- install.cmake
+++ install.cmake
@@ -27,13 +27,18 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT ENABLE_FLAT_PACKAGE)
             install_icon(${size})
         endforeach()
 
-    endif()
-elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin" AND NOT ENABLE_FLAT_PACKAGE)
-    set(ICNS_FILE ${CMAKE_BINARY_DIR}/projectMSDL.icns)
-    execute_process(COMMAND iconutil -c icns -o "${ICNS_FILE}" "${CMAKE_SOURCE_DIR}/src/resources/icons")
+        install(FILES src/resources/icons/icon_scalable.svg
+                DESTINATION ${PROJECTMSDL_ICONS_DIR}/scalable/apps
+                RENAME projectMSDL.svg
+                COMPONENT projectMSDL
+                )
 
-    install(FILES ${ICNS_FILE}
+    endif()
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin" AND NOT ENABLE_FLAT_PACKAGE)
+    install(FILES src/resources/icons/icon.icns
             DESTINATION ${PROJECTMSDL_DATA_DIR}
+            RENAME projectMSDL.icns
             COMPONENT projectMSDL
             )
 endif()
