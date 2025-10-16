b4947fb1d5efff625c09c601adb83de31b45cf56
Kai Blaschke <kai.blaschke@kb-dev.net>
2025-10-17 01:38:00 +0200

    wIP

--- packaging-windows.cmake
+++ packaging-windows.cmake
@@ -9,12 +9,12 @@ set(CPACK_PACKAGE_HOMEPAGE_URL "https://projectm-visualizer.org/")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/src/resources/gpl-3.0.txt")
 set(CPACK_STRIP_FILES TRUE)
 
-# WiX generator
+# WiX generator - requires WiX Toolset 3
 
 # Start menu entry
 set_property(INSTALL $<TARGET_FILE_NAME:projectMSDL>
         PROPERTY CPACK_START_MENU_SHORTCUTS "projectMSDL"
-)
+        )
 
 set(CPACK_WIX_UPGRADE_GUID "7d15f9b9-1122-4d5e-bc31-61bb93d7480a")
 set(CPACK_WIX_LICENSE_RTF "${CMAKE_SOURCE_DIR}/src/resources/gpl-3.0.rtf")
