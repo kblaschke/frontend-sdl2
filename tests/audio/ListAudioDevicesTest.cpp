5a59ae8b1fc3987b67a9c46ac67a1bf9c955eed4
riley-1995 <rileydenn@gmail.com>
2026-02-23 18:50:34 -0800

    Update List Audio Devices test to be more robust in checking listDevices config before and after calling ListAudioDevices().

--- tests/audio/ListAudioDevicesTest.cpp
+++ tests/audio/ListAudioDevicesTest.cpp
@@ -14,8 +14,17 @@ TEST(ListAudioDevicesTest, SetsAudioListDevicesOverrideToTrue)
     auto cfg = app.CommandLineConfiguration();
     ASSERT_TRUE(cfg); // check configuration object exists
 
+    EXPECT_FALSE(cfg->getBool("audio.listDevices", false));
+
     // Call the option handler directly (name and value are unused in implementation).
     app.ListAudioDevices("", "");
+
+    // Check to see if config has been correctly overridden
+    EXPECT_TRUE(cfg->getBool("audio.listDevices", false));
+
+    // Get config again, to test using a fresh config object
+    auto cfg = app.CommandLineConfiguration();
+    ASSERT_TRUE(cfg); // check configuration object exists
 
     EXPECT_TRUE(cfg->getBool("audio.listDevices", false));
 }
